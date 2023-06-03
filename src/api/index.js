d6ff0b81e44d13e7c0230934d4e7d215481707e7
lswlc33 <lswlc33@qq.com>
2023-06-03 22:57:41 +0800

    删除天气

--- src/api/index.js
+++ src/api/index.js
@@ -12,23 +12,6 @@ export const getHitokoto = async () => {
   return await res.json();
 };
 
-/**
- * 天气
- */
-
-// 获取高德地理位置信息
-export const getAdcode = async (key) => {
-  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
-  return await res.json();
-};
-
-// 获取高德地理天气信息
-export const getWeather = async (key, city) => {
-  const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`
-  );
-  return await res.json();
-};
 
 /**
  * 获取配置
